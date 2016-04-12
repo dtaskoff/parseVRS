58ee96ce1f12fbbdd49b7426ac862bd3aa677671
Daniel Taskoff <daniel.taskoff@gmail.com>
2016-04-12 18:33:09 +0300

    Supply readVRScene function

--- src/Parser.hs
+++ src/Parser.hs
@@ -2,6 +2,7 @@ module Parser where
 
 import Data.Attoparsec.Text.Lazy (parse, maybeResult)
 import Data.Text.Lazy (Text)
+import Data.Text.Lazy.IO as L (readFile)
 import Data.HashMap.Lazy (empty)
 import Data.Maybe (fromMaybe)
 
@@ -13,3 +14,7 @@ import Parser.VRScene (vrscene)
 parseVRScene :: Text -> VRScene
 parseVRScene = fromMaybe (VRScene empty empty) .
   maybeResult . parse vrscene
+
+-- | Parse a file containing a vrscene
+readVRScene :: FilePath -> IO VRScene
+readVRScene = fmap parseVRScene . L.readFile
