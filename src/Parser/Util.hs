a466c7691b481fe1be5aad1f9a395762d1f6d3c2
Daniel Taskoff <daniel.taskoff@gmail.com>
2016-04-12 11:54:10 +0300

    Merge Parser.Internal with Parser.Util
    No need for so complicated structure yet.

--- src/Parser/Util.hs
+++ src/Parser/Util.hs
@@ -1,8 +1,11 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Parser.Util where
 
 import Data.Attoparsec.Text
 import Data.Text (Text)
 import Data.Array.IArray (Array, listArray)
+
+import Types.Internal
 
 
 -- | Parse between two characters
@@ -37,3 +40,36 @@ listOf name p = string name *> (parens $ sepByComma p)
 -- and return an IArray containing the results
 arrayOf :: Text -> Parser a -> Parser (Array Int a)
 arrayOf name p = (subtract 1 . length >>= listArray . ((,) 0)) <$> listOf name p
+
+-- | Parse a Vector triple
+triple :: Parser (Double, Double, Double)
+triple = (\[x, y, z] -> (x, y, z)) <$>
+  listOf "Vector" double
+
+-- | Parse a Vector field
+vector :: Parser Vector
+vector = triple
+
+-- | Parse a Vector field for normal
+normal :: Parser Normal
+normal = triple
+
+-- | Parse a Vector field for colour
+colour :: Parser Colour
+colour = triple
+
+-- | Parse a ListVector field
+listVector :: Parser (Array Int (Double, Double, Double))
+listVector = arrayOf "ListVector" triple
+
+-- | Parse a ListVector containing Vector fields
+arrayVector :: Parser (Array Int Vector)
+arrayVector = listVector
+
+-- | Parse a ListVector containing Vector fields for normals
+arrayNormal :: Parser (Array Int Normal)
+arrayNormal = listVector
+
+-- | Parse a ListVector containing Vector fields for colours
+arrayColour :: Parser (Array Int Colour)
+arrayColour = listVector
